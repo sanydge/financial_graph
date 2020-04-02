053ee24ad20163ad994c482b19c96fa7c1fe7e3e
tirdeaalex <sanydge@gmail.com>
2020-04-02 13:51:38 +0300

    Candlestick Charts with Bokeh Rectangles

--- stock_analysis.py
+++ stock_analysis.py
@@ -9,17 +9,37 @@ df = data.DataReader(name="AAPL", data_source="yahoo", start=start, end=end)
 date_increase = df.index[df.Close > df.Open]
 date_decrease = df.index[df.Close < df.Open]
 
+
+def inc_dec(c, o):
+    if c > o:
+        value = "Increase"
+    elif c < o:
+        value = "Decrease"
+    else:
+        value = "Equal"
+    return value
+
+
+df['Status'] = [inc_dec(c, o) for c, o in zip(df.Close, df.Open)]
+df["Middle"] = (df.Open + df.Close)/2
+df["Height"] = abs(df.Close - df.Open)
+
 p = figure(x_axis_type="datetime", width=1000, height=300)
 p.title.text = "Candlestick Chart"
 
-hours_12 = 12*60*60*1000
+hours_12 = 12 * 60 * 60 * 1000
 
-p.rect(df.index[df.Close > df.Open], (df.Open + df.Close)/2, hours_12, abs(df.Open - df.Close),
+p.rect(df.index[df.Status == "Increase"],
+       df.Middle[df.Status == "Increase"],
+       hours_12,
+       df.Height[df.Status == "Increase"],
        fill_color='green', line_color='black')
-p.rect(df.index[df.Close < df.Open], (df.Open + df.Close)/2, hours_12, abs(df.Open - df.Close),
+p.rect(df.index[df.Status == "Decrease"],
+       df.Middle[df.Status == "Decrease"],
+       hours_12,
+       df.Height[df.Status == "Decrease"],
        fill_color='red', line_color='black')
 
 output_file("CS.html")
 show(p)
-
-
+print(df)
